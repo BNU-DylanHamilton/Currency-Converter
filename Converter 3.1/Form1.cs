5c6f83aadce3067262c149bbfc6228e15e2c95d1
Dylan Hamilton <dylan5437@hotmail.co.uk>
2020-05-01 02:29:46 +0100

    Added summaries

--- Converter 3.1/Form1.cs
+++ Converter 3.1/Form1.cs
@@ -24,6 +24,11 @@ namespace Converter_3._1
             rupees = 80;
         }
 
+        /// <summary>
+        /// This method only proceeds with 
+        /// closing the program if the user
+        /// selects 'Yes' in the message box.
+        /// </summary>
         private void closeConverter(object sender, EventArgs e)
         {
             quit = MessageBox.Show("Are you sure you want to quit?", "Check", MessageBoxButtons.YesNo);
@@ -34,18 +39,30 @@ namespace Converter_3._1
             }
         }
 
+        /// <summary>
+        /// This method converts inputted amount
+        /// to Dollars.
+        /// </summary>
         private void dollarsConversion(object sender, EventArgs e)
         {
             amount = Convert.ToDouble(poundsTextBox.Text) * dollars;
             convertedLabel.Text = amount.ToString("0.00") + " Dollars";
         }
 
+        /// <summary>
+        /// This method converts inputted amount
+        /// to Euros.
+        /// </summary>
         private void eurosConversion(object sender, EventArgs e)
         {
             amount = Convert.ToDouble(poundsTextBox.Text) * euros;
             convertedLabel.Text = amount.ToString("0.00") + " Euros";
         }
 
+        /// <summary>
+        /// This method converts inputted amount
+        /// to Rupees.
+        /// </summary>
         private void rupeesConversion(object sender, EventArgs e)
         {
             amount = Convert.ToDouble(poundsTextBox.Text) * rupees;
